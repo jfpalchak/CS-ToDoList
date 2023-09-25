cbd5bac310501908798271717924736e83ee8a1f
Joey Palchak <joey.palchak@gmail.com>
2023-09-25 15:10:30 -0700

    create ItemsController + Views/Home & Views/Items

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
@@ -10,30 +8,7 @@ namespace ToDoList.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      // We want Index to have access to _ALL_ Items:
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
       return View();
-    }
-
-    // when our form is submitted, this route will be invoked
-    // we'll change our route decorator to for this method
-    // * This route only needs to _create_ a new Item,
-    // * so it doesn't care about updating a View!
-    // * Create item, then redirect to Index, which will update the corresponding View.
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-
-      // RedirectToAction takes a route method as an argument,
-      // and tells the server to invoke that route after Create() has been invoked
-      return RedirectToAction("Index");
     }
   }
 }
---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      // We want Index to have access to _ALL_ Items:
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    // * This route only needs to _create_ a new Item,
+    // * so it doesn't care about updating a View!
+    // * Create item, then redirect to Index, which will update the corresponding View.
+    // * Note: we have two routes that use the "/items" path. We can do this because
+    // * GET and POST are two different requests! The server can tell them apart.
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+
+      // RedirectToAction takes a route method as an argument,
+      // and tells the server to invoke that route after Create() has been invoked
+      return RedirectToAction("Index");
+    }
+  }
+}
