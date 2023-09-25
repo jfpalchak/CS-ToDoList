52ee9a2062591d30c590c651b9e70811f738632f
Joey Palchak <joey.palchak@gmail.com>
2023-09-25 14:37:41 -0700

    update comments

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -29,6 +29,7 @@ namespace ToDoList.Controllers
       // the first argument here tells View to return the Index view,
       // which means we don't need to add a new Create.cshtml view to 
       // correspond with our Create() route!
+      // the second argument specifies what the Model property on the view should be
       return View("Index", myItem);
     }
   }
