a8e43ddc287df908b49ecea8fa17ef7e39e44888
Joey Palchak <joey.palchak@gmail.com>
2023-10-07 16:53:02 -0700

    create Tag AddItem view + Controller route

--- ToDoList/Controllers/TagsController.cs
+++ ToDoList/Controllers/TagsController.cs
@@ -42,5 +42,29 @@ namespace ToDoList.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult AddItem(int id)
+    {
+      Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+      ViewBag.ItemId = new SelectList(_db.Items, "ItemId", "Description");
+      return View(thisTag);
+    }
+
+    [HttpPost]
+    public ActionResult AddItem(Tag tag, int itemId)
+    {
+      // Check to see if this Item exists, or if it already has this Tag
+      #nullable enable
+      ItemTag? joinEntity = _db.ItemTags.FirstOrDefault(join => (join.ItemId == itemId && join.TagId == tag.TagId));
+      #nullable disable
+      // If it DOESN'T, AND the Item exists, add the combo to the database
+      // otherwise don't do anything
+      if (joinEntity == null && itemId != 0)
+      {
+        _db.ItemTags.Add(new ItemTag() { ItemId = itemId, TagId = tag.TagId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = tag.TagId });
+    }
   }
 }
