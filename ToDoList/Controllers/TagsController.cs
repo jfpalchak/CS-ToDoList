d477ef6b6afe466643795b699d5383dfb2d25198
Joey Palchak <joey.palchak@gmail.com>
2023-10-07 16:04:22 -0700

    add Tag Details view + Controller route

--- ToDoList/Controllers/TagsController.cs
+++ ToDoList/Controllers/TagsController.cs
@@ -20,5 +20,14 @@ namespace ToDoList.Controllers
     {
       return View(_db.Tags.ToList());
     }
+
+    public ActionResult Details(int id)
+    {
+      Tag thisTag = _db.Tags
+                        .Include(tag => tag.JoinEntities) // load each join entity, ie, list of ItemTags (references to a relationship)
+                        .ThenInclude(join => join.Item) // then, load the Item object associated with each ItemTag join entity
+                        .FirstOrDefault(tag => tag.TagId == id);
+      return View(thisTag);
+    }
   }
 }
